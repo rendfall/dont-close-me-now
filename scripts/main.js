c7a7d3cdf44ca300e3920b6dfd71affe48966c0c
rendfall <rendfall@gmail.com>
2018-02-28 01:00:47 +0100

    Refactor to es6 + clean up

--- scripts/main.js
+++ scripts/main.js
@@ -1,63 +1,98 @@
-(function (root, undefined) {
-    'use strict';
+((root, undefined) => {
+    const ONE_SECOND = 1e3;
 
-    var REFRESH_DELAY = 11 * 1000; // 11 seconds
-    var TRANSITION_DELAY = 1000; // 1000 ms of transition
-
-    var $cover = root.document.getElementById('cover');
-    var timeoutID = null;
-
-    function buildImageURL() {
-        var width = $cover.clientWidth;
-        var height = $cover.clientHeight;
-
-        return 'https://unsplash.it/' + width + '/' + height + '/?random&time=' + Date.now();
-    }
-
-    function refreshImage(cb) {
-        $cover.classList.add('loading');
-
-        // Force transition delay.
-        root.setTimeout(function () {
-            $cover.src = buildImageURL();
-
-            if (typeof cb === 'function') {
-                $cover.onload = function () {
-                    $cover.classList.remove('loading');
-                    cb.apply();
-                }
+    class App {
+        static get CONFIG() {
+            return {
+                REFRESH_DELAY: 6, // Seconds;
+                API_URL: 'https://unsplash.it'
             }
-        }, TRANSITION_DELAY);
+        };
+
+        constructor(id) {
+            this.$image = root.document.getElementById(id);
+            this.$state = root.document.getElementById('state');
+            this.loop = null;
+            this.isLoopRunning = false;
+
+            this.setupListeners();
+            this.initialize();
+        }
+
+        setupListeners() {
+            root.addEventListener('beforeunload', (evt) => {
+                const confirmationMessage = 'Do you really want to close?';
+                evt.returnValue = confirmationMessage;
+                return confirmationMessage;
+            });
+
+            root.addEventListener('click', () => {
+                this.isLoopRunning
+                    ? this.stopLoop()
+                    : this.startLoop();
+            })
+        }
+
+        initialize() {
+            this.startLoop();
+        }
+
+        buildImageUrl() {
+            const { clientWidth, clientHeight } = this.$image;
+            const now = Date.now();
+            return `https://unsplash.it/${clientWidth}/${clientHeight}/?random&time=${now}`;
+        }
+
+        renderImage(src) {
+            const { $image } = this;
+
+            $image.classList.add('loading');
+            $image.addEventListener('load', () => {
+                $image.classList.remove('loading');
+            });
+            $image.src = src;
+        }
+
+        startLoop() {
+            const { REFRESH_DELAY } = App.CONFIG;
+
+            let accumulatedTime = 0;
+            let lastTime = 0;
+
+            const nextStep = (deltaTime) => {
+                accumulatedTime += (deltaTime - lastTime) / ONE_SECOND;
+                lastTime = deltaTime;
+
+                if (accumulatedTime > REFRESH_DELAY) {
+                    this.clockTick(lastTime / ONE_SECOND);
+                    accumulatedTime = 0;
+                }
+
+                this.loop = root.requestAnimationFrame(nextStep);
+            };
+
+            this.clockTick(0);
+            this.loop = root.requestAnimationFrame(nextStep);
+            this.isLoopRunning = true;
+            this.$state.classList.remove('paused');
+        }
+
+        stopLoop() {
+            window.cancelAnimationFrame(this.loop);
+            this.isLoopRunning = false;
+            this.$state.classList.add('paused');
+        }
+
+        clockTick(time) {
+            const imageSrc = this.buildImageUrl();
+            this.renderImage(imageSrc);
+        }
     }
 
-    function loop() {
-        root.clearTimeout(timeoutID);
-
-        refreshImage(function () {
-            timeoutID = root.setTimeout(loop, REFRESH_DELAY);
-        });
-    }
-
-    root.addEventListener('load', function () {
-        loop();
-
-        $cover.addEventListener('click', function () {
-            loop();
-        });
+    root.addEventListener('DOMContentLoaded', () => {
+        new App('image');
+    }, {
+        passive: true,
+        once: true
     });
-
-    root.addEventListener('blur', function () {
-        root.clearTimeout(timeoutID);
-    });
-
-    root.addEventListener('focus', function () {
-        loop();
-    });
-
-    root.addEventListener('beforeunload', function (e) {
-        var confirmationMessage = 'Do you really want to close?';
-
-        e.returnValue = confirmationMessage;
-        return confirmationMessage;
-    });
-})(window);
+})(this);
